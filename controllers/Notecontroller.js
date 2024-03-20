b9ee0a78fd86e5426bfd13ebd9fc5ce95b7d8f8b
laitouss-yassine <yassinelaitouss2380@gmail.com>
2024-03-20 14:25:46 +0000

    fix update and delete  methods

--- controllers/Notecontroller.js
+++ controllers/Notecontroller.js
@@ -1,35 +1,74 @@
 const Notes = require('../model/Noteschema');
 
 const Notecontroller = {
-    postNote : async(req,res)=>{
-        const {Title, Description}= req.body;
-        try{
+    postNote: async (req, res) => {
+        const { Title, Description } = req.body;
+        try {
 
-            const newPost = await Notes.create({Title,Description})
+            const newPost = await Notes.create({ Title, Description })
             res.json(newPost);
             console.log('note created successfully');
-        }catch(err){
+        } catch (err) {
             console.log('error creating note: ' + err.message)
         }
     },
-    getAllNote : async(req,res)=>{
-        try{
+    getAllNote: async (req, res) => {
+        try {
             const note = await Notes.find();
             res.json(note);
-        }catch(err){
-            console.log('error getting note:'+ err.message)
+        } catch (err) {
+            console.log('error getting note:' + err.message)
         }
     },
-    getOneNote: async(req,res)=>{
-       try{
-        const {id} = req.params;
-        const note = await Notes.findById(id);
-        res.status(200).json(note);
+    getOneNote: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const note = await Notes.findById(id);
+            res.status(200).json(note);
 
-       }catch(error){
-        console.log('error getting note:'+ error.message)
-       }
-    }
+        } catch (error) {
+            console.log('error getting note:' + error.message)
+        }
+    },
+    updateNote: async (req, res) => {
+        const { id } = req.params;
+        const { Title, Description } = req.body;
+        try {
+           
+            await Notes.findByIdAndUpdate(id, { Title, Description });
+            res.status(200).send({
+                message: "Task Updated Successfully"
+            })
+        } catch (error) {
+            return res.status(500).send({
+                message: "Oops something went wrong",
+            })
+        }
+    },
+    deleteNote: async (req,res) => {
+        try {
+          const { id } = req.params;
+      
+          const taskUpdate = {
+            deleted: true
+          }
+      
+          const note = await Notes.findByIdAndDelete(id, taskUpdate)
+          if(note) {
+              return res.status(200).send({
+                message: "Task deleted successufly",
+              });
+
+          }
+          return res.json({
+            message: "note not found"
+          })
+      
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ error: 'Oops something went wrong' });
+        }
+      }
 }
 
 
--- routes/Noteroutes.js
+++ routes/Noteroutes.js
@@ -9,4 +9,8 @@ router.get('/notes', Notecontroller.getAllNote);
 
 router.get('/notes/:id', Notecontroller.getOneNote);
 
+router.put('/notes/:id', Notecontroller.updateNote);
+
+router.delete('/notes/:id', Notecontroller.deleteNote);
+
 module.exports=  router;
